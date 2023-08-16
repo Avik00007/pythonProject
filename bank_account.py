82557e834224be6a2293aaead44bc57cc073b6b7
Avik bhattacharyya <65553676+Avik00007@users.noreply.github.com>
2023-08-16 14:18:57 +0530

    Update bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,27 +1,29 @@
+print("welcome!!!!")
 class BankAccount:
-    __account_number=None
-    __balance=None
+    _CurBal = None
+    _AccNo = None
+    def __init__(self,num,IniAmount):
+        self._CurBal = IniAmount
+        self._AccNo = num
 
-    def __init__(self, account_number, balance):
-        self.account_number = account_number
-        self.balance = balance
+    def DisplayBalance(self):
+        print(f"{self._AccNo}your account balence is {self._CurBal}")
 
     def deposit(self, amount):
-        self.balance += amount
+        self._CurBal += amount
+        print(f"{amount} is deposited in your account: {self._AccNo}")
 
-    def show_balance(self):
-        print(str(self.account_number)+" Your balance is: Rs{} ".format(self.balance))
+    def ChkBal(self,amount):
+        if self._CurBal < amount:
+            return False
+        else:
+            return True
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient funds.")
-        else:
-            self.balance -= amount
+        if self.ChkBal(amount):
+            self._CurBal -= amount
+            print (f"{amount}is withdraw from your account: {self._AccNo}")
 
-    def transfer(self, to_account, amount, bankAccount):
-
+    def Transfer(self,amount, BankAccount):
         self.withdraw(amount)
-        bankAccount.deposit(amount)
-
-
-
+        BankAccount.deposit(amount)
